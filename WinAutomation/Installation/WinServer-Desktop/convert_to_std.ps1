2a62b5f9f76e107c2488e836fbd78cbf14f88bc2
KR-sew <lanbak43@gmail.com>
2025-04-02 19:19:23 +0300

    edit convert_to_std.ps1

--- WinAutomation/Installation/WinServer-Desktop/convert_to_std.ps1
+++ WinAutomation/Installation/WinServer-Desktop/convert_to_std.ps1
@@ -3,11 +3,11 @@ $edition = "ServerStandard"
 $productKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX" # Replace with a valid key
 
 Write-Host "Checking Current Edition..."
-$currentEdition = (DISM /Online /Get-CurrentEdition | Select-String "Current Edition").ToString()
+$currentEdition = (DISM /Online /Get-CurrentEdition | Select-String "Current Edition").Matches.Groups[0].Value.Trim()
 Write-Host "Current Edition: $currentEdition"
 
 Write-Host "Checking Available Target Editions..."
-$targetEditions = (DISM /Online /Get-TargetEditions | Select-String "Target Edition").ToString()
+$targetEditions = (DISM /Online /Get-TargetEditions | Select-String "Target Edition" | ForEach-Object { $_.Line.Trim() })
 Write-Host "Available Target Editions: $targetEditions"
 
 if ($targetEditions -match $edition) {
