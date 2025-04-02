6e1987012e75c29b5915b555eeddff79312a559d
KR-sew <lanbak43@gmail.com>
2025-04-02 19:08:27 +0300

    add Convert-EditionParam.ps1

---
+++ WinAutomation/Installation/WinServer-Core/Convert-EditionParam.ps1
@@ -0,0 +1,28 @@
+# Convert Windows Server Evaluation to Standard Edition
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$edition,  # Name of the VM
+
+    [Parameter(Mandatory = $true)]
+    [string]$productKey # Product key for converting
+)
+
+$edition = "ServerStandard"
+$productKey = "TVRH6-WHNXV-R9WG3-9XRFY-MY832" # Replace with a valid key
+
+Write-Host "Checking Current Edition..."
+$currentEdition = (DISM /Online /Get-CurrentEdition | Select-String "Current Edition").Matches.Groups[0].Value.Trim()
+Write-Host "Current Edition: $currentEdition"
+
+Write-Host "Checking Available Target Editions..."
+$targetEditions = (DISM /Online /Get-TargetEditions | Select-String "Target Edition" | ForEach-Object { $_.Line.Trim() })
+Write-Host "Available Target Editions: $targetEditions"
+
+if ($targetEditions -match $edition) {
+    Write-Host "Upgrading to $edition..."
+    DISM /Online /Set-Edition:$edition /ProductKey:$productKey /AcceptEula
+    Write-Host "Upgrade completed. Rebooting now..."
+    shutdown /r /t 10
+} else {
+    Write-Host "Error: Cannot upgrade to $edition. Check available editions."
+}
